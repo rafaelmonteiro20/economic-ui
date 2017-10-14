ab8a9cf2beb953e88ae754692eb15e0335cf373b
Rafael Monteiro <rafajapones@gmail.com>
2017-10-13 21:28:03 -0300

    Adicionando botão de seleção

--- src/app/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class LancamentoCadastroComponent {
 
- 
+  tipos = [
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' }
+  ];
 
 }
