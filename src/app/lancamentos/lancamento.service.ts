53c7e4227bf43915a3533ad7a11c4ff25d94ab18
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2018-06-07 22:05:05 -0300

    Pesquisando lançamentos por descrição

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -3,6 +3,10 @@ import { Http } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
 
+export class LancamentoFilter {
+  descricao: string;
+}
+
 @Injectable()
 export class LancamentoService {
 
@@ -10,8 +14,14 @@ export class LancamentoService {
 
   constructor(private http: Http) { }
 
-  pesquisar(): Promise<any> {
-    return this.http.get(this.lancamentosUrl)
+  pesquisar(filtro: LancamentoFilter): Promise<any> {
+    const params = new URLSearchParams();
+
+    if(filtro.descricao) {
+      params.set('descricao', filtro.descricao);
+    }
+
+    return this.http.get(this.lancamentosUrl, { search: filtro })
                .toPromise()
                .then(response => response.json().content);
   }
