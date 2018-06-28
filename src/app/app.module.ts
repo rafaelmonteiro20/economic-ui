269a9925ca6562dadf93a9636b37b7d10b995fc1
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2018-06-27 21:46:13 -0300

    Organizando importações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ToastyModule } from 'ng2-toasty';
-import { ConfirmDialogModule } from 'primeng/components/confirmdialog/confirmdialog';
-import { ConfirmationService } from 'primeng/components/common/confirmationservice';
-
 import { AppComponent } from './app.component';
 
 import { CoreModule } from './core/core.module';
 import { CategoriasModule } from './categorias/categorias.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
-
-import { LancamentoService } from './lancamentos/lancamento.service';
-import { PessoaService } from './pessoas/pessoa.service';
 
 import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
 import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
@@ -41,20 +34,13 @@ const routes: Routes = [
     FormsModule,
     HttpModule,
     RouterModule.forRoot(routes),
-
-    ToastyModule.forRoot(),
-    ConfirmDialogModule,
-
+    
     CoreModule,
     CategoriasModule,
     LancamentosModule,
     PessoasModule
   ],
-  providers: [
-    LancamentoService,
-    PessoaService,
-    ConfirmationService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ToastyModule } from 'ng2-toasty';
+import { ConfirmDialogModule } from 'primeng/components/confirmdialog/confirmdialog';
+import { ConfirmationService } from 'primeng/components/common/confirmationservice';
+
+import { LancamentoService } from './../lancamentos/lancamento.service';
+import { PessoaService } from './../pessoas/pessoa.service';
+import { ErrorHandlerService } from './error-handler.service';
+
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ToastyModule.forRoot(),
+    ConfirmDialogModule,
   ],
   declarations: [
     NavbarComponent
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    ToastyModule,
+    ConfirmDialogModule
+  ],
+  providers: [
+    LancamentoService,
+    PessoaService,
+    ConfirmationService,
+    ErrorHandlerService
   ]
 })
 export class CoreModule { }
