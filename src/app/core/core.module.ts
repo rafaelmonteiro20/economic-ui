4ff0c4babf63ea2f75d2c01e2e814013d8f551da
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2018-08-19 09:29:17 -0300

    Decodificando o token Jwt

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { ConfirmDialogModule } from 'primeng/components/confirmdialog/confirmdialog';
 import { ConfirmationService } from 'primeng/components/common/confirmationservice';
 import { ToastyModule } from 'ng2-toasty';
+import { JwtHelper } from 'angular2-jwt';
 
 import { AuthService } from '../seguranca/auth.service';
 import { LancamentoService } from './../lancamentos/lancamento.service';
@@ -38,6 +39,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     CategoriaService,
     ConfirmationService,
     ErrorHandlerService,
+    JwtHelper,
     { provide: LOCALE_ID, useValue: 'pt-BR' }
   ]
 })
--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import { Http, Headers } from '../../../node_modules/@angular/http';
 
 import 'rxjs/add/operator/toPromise';
+import { JwtHelper } from '../../../node_modules/angular2-jwt';
 
 @Injectable()
 export class AuthService {
 
-  oauthTokenUrl = 'http://localhost:8080/oauth/token';
+  oauthTokenUrl = 'http://localhost:8081/oauth/token';
+  jwtPayload: any;
 
-  constructor(private http: Http) {
-
+  constructor(private http: Http, private jwtHelper: JwtHelper) { 
+    this.carregarToken();      
   }
 
   login(usuario:string, senha:string) : Promise<void> {
@@ -24,10 +26,25 @@ export class AuthService {
       .toPromise()
       .then(response => {
         console.log(response);
+        this.armazenarToken(response.json().access_token)
       })
       .catch(response => {
         console.log(response);
       });
+  }
+
+  private armazenarToken(token: string) {
+    this.jwtPayload = this.jwtHelper.decodeToken(token);
+    localStorage.setItem('token', token);
+  }
+
+  private carregarToken() {
+    const token = localStorage.getItem('token');
+  
+    if(token) {
+      this.armazenarToken(token);
+    }
+  
   }
 
 }
