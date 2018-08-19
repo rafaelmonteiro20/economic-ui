56ad9e044a6f345aae819a7f096c891400b01d4a
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2018-08-19 10:28:03 -0300

    Tratando erros no login

--- src/app/seguranca/login/login.component.ts
+++ src/app/seguranca/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '../../../../node_modules/@angular/platform-browser';
+import { Router } from '../../../../node_modules/@angular/router';
 
 import { AuthService } from '../auth.service';
+import { ErrorHandlerService } from '../../core/error-handler.service';
 
 @Component({
   selector: 'app-login',
@@ -12,13 +14,18 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private auth: AuthService,
-    private title: Title ) { }
+    private title: Title,
+    private router: Router,
+    private errorHandler: ErrorHandlerService) { }
 
   ngOnInit() {
+    this.title.setTitle('Login');
   }
 
   logar(email: string, senha: string) {
-    this.auth.login(email, senha);
+    this.auth.login(email, senha)
+      .then(() => this.router.navigate(['/lancamentos']))
+      .catch(error => this.errorHandler.handle(error));
   }
 
 }
