5a6181d22ad7be2ec695ae3bd0e98164f77937a5
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2018-09-16 14:55:28 -0300

    Criando gráficos com dados estáticos

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PanelModule } from 'primeng/panel';
+import { ChartModule } from 'primeng/chart';
+
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
@@ -8,6 +11,9 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   imports: [
     CommonModule,
+
+    PanelModule,
+    ChartModule,
 
     SharedModule,
     HomeRoutingModule
